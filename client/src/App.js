6bd608dc9b5051ee900cd07b63fbf62fea9decb6
PalomaSenpai <aguilaproplayer@gmail.com>
2025-03-19 18:45:01 -0600

    Login Full

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Importa Router, Routes y Route
 import LoginPage from './componentes/LoginPage';
+import HomePage from './componentes/HomePage';
 
 function App() {
   const [backendData, setBackendData] = useState([{}]);
@@ -17,6 +18,7 @@ function App() {
     <Router> {/* Envuelve todo en BrowserRouter */}
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage/>}/>
         {/* Otras rutas pueden ir aquí */}
       </Routes>
     </Router>
